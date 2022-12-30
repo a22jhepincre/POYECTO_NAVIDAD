911aff4ba300478ef4afa5456d9de79d87c987a4
premi <premi@DESKTOP-LGKAOAA.home>
2022-12-30 16:58:47 +0100

    Actualizando

--- Proyecto Navidad/src/proyecto/navidad/ProyectoNavidad.java
+++ Proyecto Navidad/src/proyecto/navidad/ProyectoNavidad.java
@@ -1,45 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto.navidad;
 
-/**
- *
- * @author premi
- */
+import java.util.Scanner;
+import java.util.Random;
+
 public class ProyectoNavidad {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner s = new Scanner(System.in);
+    public static Random rnd = new Random();
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Prueba 1
-        //prueba 2
-        for(int i = 0; i < 10; i++){
-            System.out.println("hola");
-            if(i < 5){
-                System.out.println("tonto");
-            }
-            else{
-                System.out.println("");
-            }
-        }
-        boolean salir = false;
-        int i = 0;
-        while(!salir){
-            System.out.println("Pasa el codigo.");
-            if(i == 10)
-                salir = true;
-            i++;
-        }
-        String palabras []= new String [10];
-        for (int j=0;j<10;j++){
-            System.out.println("ERROR MARICON");
-        }
-        //aaaaaa
-        //bfdbdffghhddhf
+
+        menu();
     }
-    
+
+    static void menu() {
+        boolean salir = false;
+        do {
+
+            System.out.println("1. Realizar sorteo.\n2. Comprobar mi numero.\n3. Salir\n");
+            int opcion = escanearEntero("Selecciona una opcion: ");
+            switch (opcion) {
+                case 1 ->
+                    sorteig();
+                case 3 ->
+                    salir = true;
+                default ->
+                    System.out.println("Opcion invalida. Prueba de nuevo");
+            }
+        } while (!salir);
+    }
+
+    static int escanearEntero(String mtj) {
+        int num;
+        System.out.print(mtj);
+        while (!s.hasNextInt()) {
+            s.next();
+            System.out.println("Ha de ser enter.");
+            System.out.print(mtj);
+        }
+        num = s.nextInt();
+        return num;
+    }
+
+    static void sorteig() {
+
+        final int MAX = 13;
+        int[] premiosGordos = new int[MAX];
+        int[] quintoPremio = new int[8];
+        int aux = 0;
+        
+        for (int i = 0; i < MAX; i++) {
+            premiosGordos[i] = rnd.nextInt(100000);
+            for (int j = 0; j < i; j++) {
+                while (premiosGordos[i] == premiosGordos[j]) {
+                    premiosGordos[i] = rnd.nextInt(100000);
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX; i++) {
+            if (i == 0) {
+                int primerPremio = premiosGordos[i];
+                System.out.println("Premio Gordo: 4.000.000 --> " + premiosGordos[i]);
+            }
+            if (i == 1) {
+                int segundoPremio = premiosGordos[i];
+                System.out.println("Segundo premio: 1.250.000 --> " + premiosGordos[i]);
+            }
+            if (i == 2) {
+                int tercerPremio = premiosGordos[i];
+                System.out.println("Tercer Premio: 500.000 --> " + premiosGordos[i]);
+            }
+            if (i == 3) {
+                int[] cuartoPremio = new int[2];
+                cuartoPremio[0] = premiosGordos[i];
+                cuartoPremio[1] = premiosGordos[i + 1];
+                System.out.println("Cuarto premio : 200.000 --> " + cuartoPremio[0] + ", " + cuartoPremio[1]);
+            }
+            if (i > 4) {
+                {
+                    quintoPremio[aux] = premiosGordos[i];
+                    aux++;
+                }
+
+            }
+        }
+        System.out.print("Quinto premio : 60.000 --> ");
+        for (int k = 0; k < quintoPremio.length-1; k++) {
+            System.out.print(quintoPremio[k] + ", ");
+        }
+        System.out.println(quintoPremio[quintoPremio.length-1]);
+    }
+
 }
